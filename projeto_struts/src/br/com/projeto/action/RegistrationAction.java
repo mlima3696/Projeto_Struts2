8310fbe1e8811a2b3c5ec9ded6f16dc192459bd5
mlima3696 <mjuniorlima3@gmail.com>
2020-10-11 16:43:34 -0300

    Struts2-Aula10-Erro

--- projeto_struts/src/br/com/projeto/action/RegistrationAction.java
+++ projeto_struts/src/br/com/projeto/action/RegistrationAction.java
@@ -2,11 +2,11 @@ package br.com.projeto.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class IndexAction extends ActionSupport{
+public class RegistrationAction extends ActionSupport{
 
 	@Override
 	public String execute() throws Exception {
-		System.out.println("Executou Index");
+		System.out.println("Executou registration");
 		return "success";
 	}
 
