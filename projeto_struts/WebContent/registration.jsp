d6841b2c924875c5b2063cfa9a8f1c209b0bafa0
mlima3696 <mjuniorlima3@gmail.com>
2020-10-24 15:45:11 -0300

    Struts2-Aula13

--- projeto_struts/WebContent/registration.jsp
+++ projeto_struts/WebContent/registration.jsp
@@ -1,12 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@taglib uri="/struts-tags" prefix="s"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Struts2daTristeza</title>
+<title>Insert title here</title>
 </head>
 <body>
-<h1>Erro! Page</h1>
+
 </body>
 </html>
--- projeto_struts/src/br/com/projeto/action/RegistrationAction.java
+++ projeto_struts/src/br/com/projeto/action/RegistrationAction.java
@@ -3,7 +3,23 @@ package br.com.projeto.action;
 import com.opensymphony.xwork2.ActionSupport;
 
 @SuppressWarnings("serial")
-public class RegistrationAction extends ActionSupport{
+public class RegistrationAction extends ActionSupport {
+
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void validation() {
+		if ("".equals(getUsername())) {
+			addFieldError("Username", getText("username.required"));
+		}
+	}
 
 	@Override
 	public String execute() throws Exception {
